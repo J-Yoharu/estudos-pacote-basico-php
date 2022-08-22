a281727b76a6e88b1d9f707fff06c077a360d374
jonathas Augusto <jonathas_a.a@hotmail.com>
2022-08-22 15:56:47 -0300

    First commit

---
+++ src/Teste.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Basico;
+
+class Teste 
+{
+    public function hello() 
+    {
+        return 'world';
+    }
+
+    public static function world()
+    {
+        return 'hello';
+    }
+}
+
